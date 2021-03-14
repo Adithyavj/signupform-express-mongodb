23d09f1537158892c55b488a87e1f53b158cabfa
Adithya Vijay K <adithyavj@gmail.com>
2021-03-14 22:17:45 +0530

    mongodb connection string

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var MongoClient = require("mongodb").MongoClient;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.post("/submit", function (req, res) {
+  console.log(req.body);
+  MongoClient.connect("mongodb://localhost:27017", function (err, client) {
+    if (err) 
+      console.log("error");
+    else 
+      console.log("Database created!");
+    //client.close();
+  });
+  res.send("got it");
 });
 
 module.exports = router;
